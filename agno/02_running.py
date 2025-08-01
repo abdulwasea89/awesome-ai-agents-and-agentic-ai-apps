5a8e4ea91dece2decf4a99a2e0e8f2adb3401c62
abdulwasea89 <abdulwasea853@gmail.com>
2025-08-02 00:27:53 +0500

    New file

---
+++ agno/02_running.py
@@ -0,0 +1,3 @@
+# Running Your Agent 
+# The Agent.run() func(x) runs the agent and generate the response, either as a RunResponse object or a stream of RunResponse
+# You see in the exmaple of Agno agent there agent.print_response() which is a helper utility to print res in the shell/terminal. it uses agent.run() under the hood
